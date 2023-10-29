471cd802485c12c12e7631552197947119d27f93
ka <todo1@rotarydef.net>
2023-10-28 23:54:39 -0700

    cmake

--- sharedlib.cmake
+++ sharedlib.cmake
@@ -1,18 +1,14 @@
 
-if(NOT DEFINED KAUTIL_THIRD_PARTY_DIR)
-    set(KAUTIL_THIRD_PARTY_DIR ${CMAKE_BINARY_DIR})
-    file(MAKE_DIRECTORY "${KAUTIL_THIRD_PARTY_DIR}")
+
+set(${PROJECT_NAME}_m_evacu ${m})
+set(m CMakePullLocalRepositoryAsSymLink)
+list(APPEND ${m}_unsetter )
+
+
+if(NOT EXISTS ${CMAKE_BINARY_DIR}/CMakeKautilHeader.cmake)
+    file(DOWNLOAD https://raw.githubusercontent.com/kautils/CMakeKautilHeader/v0.0.1/CMakeKautilHeader.cmake ${CMAKE_BINARY_DIR}/CMakeKautilHeader.cmake)
 endif()
-
-macro(git_clone url)
-    get_filename_component(file_name ${url} NAME)
-    if(NOT EXISTS ${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name})
-        file(DOWNLOAD ${url} "${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name}")
-    endif()
-    include("${KAUTIL_THIRD_PARTY_DIR}/kautil_cmake/${file_name}")
-    unset(file_name)
-endmacro()
-
+include(${CMAKE_BINARY_DIR}/CMakeKautilHeader.cmake)
 git_clone(https://raw.githubusercontent.com/kautils/CMakeLibrarytemplate/v0.0.1/CMakeLibrarytemplate.cmake)
 
 set(module_name sharedlib)
@@ -42,3 +38,9 @@ add_executable(${__t})
 target_sources(${__t} PRIVATE ${CMAKE_CURRENT_LIST_DIR}/unit_test.cc)
 target_link_libraries(${__t} PRIVATE ${${module_name}_static})
 target_compile_definitions(${__t} PRIVATE ${${module_name}_static_tmain_ppcs})
+
+foreach(__v ${${m}_unsetter})
+    unset(${__v})
+endforeach()
+unset(${m}_unsetter)
+set(m ${${PROJECT_NAME}_m_evacu})
